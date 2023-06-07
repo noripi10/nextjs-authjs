e5ed70fe8fceb132130e40129a48de5e96a10fa0
sugiyama <sgym.snk@gmail.com>
2023-06-07 17:25:57 +0900

    update env key

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -48,7 +48,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
-  secret: process.env.SECRET ?? '',
+  secret: process.env.NEXTAUTH_SECRET ?? '',
   debug: process.env.NODE_ENV !== 'production',
 };
 
