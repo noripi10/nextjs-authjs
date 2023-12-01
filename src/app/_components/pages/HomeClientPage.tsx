de98a58e93cefbaaebc52a191f2ad0790002f9d0
sugiyama <sgym.snk@gmail.com>
2023-12-01 13:55:25 +0900

    update image fit same size,

--- src/app/_components/pages/HomeClientPage.tsx
+++ src/app/_components/pages/HomeClientPage.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { Button, Center, Container, Flex, HStack, SimpleGrid, VStack, useMediaQuery } from '@chakra-ui/react';
+import { Box, Button, Center, Container, Flex, HStack, SimpleGrid, VStack, useMediaQuery } from '@chakra-ui/react';
 
 import { AuthSession } from '../../_components/AuthSession';
 
@@ -13,6 +13,7 @@ export default function HomeClientPage({ resources }: { resources: any }) {
   const [isSM] = useMediaQuery('(min-width: 640px)');
   const columns = is2XL ? 4 : isXL ? 3 : isSM ? 2 : 1;
   console.info({ columns });
+
   return (
     // main
     <Flex as='main' minH={'100vh'} flexDir={'column'} bgColor={'gray.50'} _dark={{ bgColor: 'gray.600' }}>
@@ -34,28 +35,40 @@ export default function HomeClientPage({ resources }: { resources: any }) {
       </Container>
 
       {/* @ts-ignore */}
-      <SimpleGrid columns={columns} maxW={'1536px'} margin={'auto'}>
+      <SimpleGrid columns={columns} maxW={'1536px'} margin={'auto'} rowGap={'1'} columnGap={2}>
         {/* @ts-ignore */}
         {resources.map((row) => {
-          console.info({ row });
+          // console.info({ row });
           // @ts-ignore
           return (
-            <Image
+            <Box
+              as='figure'
               key={row.asset_id}
-              alt={'photo sample cloudinary'}
-              // src={row.url}
-              // @ts-ignore
-              src={`https://res.cloudinary.com/dn27wvjci/image/upload/c_scale,w_360/${row.public_id}.${row.format}`}
-              style={{ transform: 'translate3d(0, 0, 0)' }}
-              objectFit='cover'
-              // placeholder='blur'
-              width={720}
-              height={480}
-              sizes='(max-width: 640px) 100vw,
-                  (max-width: 1280px) 50vw,
-                  (max-width: 1536px) 33vw,
-                  25vw'
-            />
+              position={'relative'}
+              transition={'all'}
+              bg='gray.800'
+              w='96'
+              h='96'
+              border={'1px solid'}
+            >
+              <Image
+                key={row.asset_id}
+                alt={'photo sample cloudinary'}
+                // src={row.url}
+                // @ts-ignore
+                src={`https://res.cloudinary.com/dn27wvjci/image/upload/c_scale,w_360/${row.public_id}.${row.format}`}
+                // style={{ transform: 'translate3d(0, 0, 0)' }}
+                objectFit='cover'
+                // placeholder='blur'
+                // width={480}
+                // height={480}
+                fill
+                sizes='(max-width: 640px) 100vw,
+                    (max-width: 1280px) 50vw,
+                    (max-width: 1536px) 33vw,
+                    25vw'
+              />
+            </Box>
           );
         })}
       </SimpleGrid>
